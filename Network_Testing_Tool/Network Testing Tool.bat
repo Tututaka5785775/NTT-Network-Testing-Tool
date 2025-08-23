6cc42d714839ec7e11203183c4cc29df496877b0
Tututaka5785775 <hilmitayanckasarci77753@gmail.com>
2025-08-23 09:20:18 +0300

    Add files via upload

---
+++ Network_Testing_Tool/Network Testing Tool.bat
@@ -0,0 +1,61 @@
+@echo off
+color 0a
+title Network Testing Tool
+cls
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo PLEASE RUN THIS SCRIPT AS ADMINISTRATOR!
+    title PLEASE RUN THIS SCRIPT AS ADMINISTRATOR!
+	pause
+    exit /b
+)
+
+arp -a
+arp -a | findstr /v "Interface"
+
+pause
+
+netstat -ano
+netstat -ano | findstr "LISTENING"
+netstat -n                           
+netstat -r
+
+pause
+
+getmac
+
+pause
+
+ipconfig /all | findstr "Physical Address"
+ipconfig /all | findstr "DHCPv6 Client DUID"
+ipconfig /all | findstr "DNS Servers"
+ipconfig /all | findstr "Link-local IPv6 Address"
+ipconfig /all | findstr "Default Gateway"
+ipconfig /all | findstr "IPv4"
+ipconfig /all | findstr "DHCP Servers"
+ipconfig /all | findstr /C:"Subnet Mask"
+
+pause
+
+echo -----------System info-----------
+pause
+
+systeminfo
+
+wmic cpu get loadpercentage
+
+wmic OS get FreePhysicalMemory
+pause
+
+echo -----------Ping Test-----------
+echo Warning This Can Take 3-4 mins
+pause > nul
+
+ping -n 4 8.8.8.8
+
+ping google.com
+
+tracert google.com
+
+pause > nul
---
+++ Network_Testing_Tool/README.txt
@@ -0,0 +1,2 @@
+before using the software
+please run the bat file as administrator. To see all the information and for security
